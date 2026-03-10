188b5737bf065d5a32c1271af9cb83265565d5d8
LUIZ HENRIQUE ANDRADE SANTOS <andradesantos132019@gmail.com>
2026-03-09 22:42:12 -0300

    aula 09/03

--- src/pages/sobre/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Sobre(){
-    return(
-        <>
-        <h1>Sobre</h1>
-        
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maxime aut pariatur, aliquam nesciunt officiis cupiditate porro laborum consequuntur nam perferendis rem sequi veniam perspiciatis? Cum omnis minus dolore odio.</p>
-        </>
-    )
-}
