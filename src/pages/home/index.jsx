eea0502ed4a3186da97eb7e9b8a2f0a38f00ab71
LUIZ HENRIQUE ANDRADE SANTOS <andradesantos132019@gmail.com>
2026-03-13 22:44:24 -0300

    Aula 13/03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,24 @@
+import { Link } from 'react-router'
+import styles from './index.module.css'
+
 function Home() {
 
   return (
-    <>
-      <h1>Aula Introdução VITE JS</h1>
-      <h2>Preparo do projeto</h2>
-      <h3>Estilização</h3>
-      <p>Entendendo uso de css dentro do projeto</p>
-      <label>Exemplo Label</label>
-      <small>Exemplo Small</small>
-      </>
+    <div className={styles.container}>
+
+      <div className={styles.containerLista}>
+        <h1>Exemplos</h1>
+        <Link to='/exemplo/1'>Exemplo 1</Link>
+        <Link to='/exemplo/2'>Exemplo 2</Link>
+      </div>
+
+      <div className={styles.containerLista}>
+        <h1>Atividades</h1>
+        
+      </div>
+
+    </div>
+
   )
 }
 
